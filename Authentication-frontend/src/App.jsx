e8db117331996b8ab74e18452c27c31dbb51f810
SADAT MIIRO <miirosadat0@gmail.com>
2026-09-14 16:08:53 +0300

    Add public layout and header components

--- Authentication-frontend/src/App.jsx
+++ Authentication-frontend/src/App.jsx
@@ -1,10 +1,18 @@
+import { useEffect, useState } from 'react';
+
 function App() {
-    return (
-        <div className="justify-center items-center">
-            <h1 className="text-center font-bold underline">This is it</h1>
-            <button>Button</button>
-        </div>
-    )
+    const [status, setStatus] = useState('checking...');
+
+
+
+    useEffect(() => {
+        fetch('http://localhost:8080')
+            .then((res) => res.json())
+            .then((data) => setStatus(data.status))
+            .catch((err) => setStatus('ERROR: ' + err.message));
+    }, []);
+
+    return <h1>Backend status: {status}</h1>;
 }
 
 export default App;

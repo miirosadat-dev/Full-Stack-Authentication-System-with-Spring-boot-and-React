9f0039e308e848ef69ec5b4cc660ab21bc765b0f
SADAT MIIRO <miirosadat0@gmail.com>
2026-09-14 14:43:33 +0300

    Add initial commit and SecurityFilterChain

---
+++ Authentication-frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="justify-center items-center">
+            <h1 className="text-center font-bold underline">This is it</h1>
+            <button>Button</button>
+        </div>
+    )
+}
+
+export default App;
--- yyoo/app.js
+++
@@ -1,9 +0,0 @@
-let a = 4;
-let b = 7;
-
-function count() {
-    let x = a * b;
-    console.log(x);
-}
-
-console.log(count());

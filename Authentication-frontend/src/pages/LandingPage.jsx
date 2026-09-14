554963137251d959d55ac40f38b9b0648faed7da
SADAT MIIRO <miirosadat0@gmail.com>
2026-09-14 16:14:23 +0300

    Add landing page

---
+++ Authentication-frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import PublicLayout from '../layouts/PublicLayout';
+import Hero from '../components/Hero';
+
+function Landing() {
+    return (
+        <PublicLayout>
+            <Hero />
+        </PublicLayout>
+    );
+}
+
+export default Landing;

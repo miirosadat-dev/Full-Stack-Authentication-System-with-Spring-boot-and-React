b5a831e76520b26fe3e0208998c6dc4a0bc10f1d
SADAT MIIRO <miirosadat0@gmail.com>
2026-09-17 00:33:56 +0300

    Add the Custom user details service class

---
+++ Authentication-backend/auth/src/main/java/com/sadat/auth/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.sadat.auth.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.sadat.auth.entity.User;
+import com.sadat.auth.repository.UserRepository;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("No account with that email"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPasswordHash())
+                .authorities(Collections.emptyList())
+                .build();
+    }
+}

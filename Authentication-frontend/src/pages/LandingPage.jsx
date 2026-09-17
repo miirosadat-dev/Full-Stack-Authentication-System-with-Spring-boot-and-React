78c4c6884006fc7d62e06878504bfe0181e3b609
SADAT MIIRO <miirosadat0@gmail.com>
2026-09-18 01:11:47 +0300

    Udate changes in LandingPage and App components

--- Authentication-frontend/src/pages/LandingPage.jsx
+++ Authentication-frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,9 @@
 import PublicLayout from '../layouts/PublicLayout';
 import Hero from '../components/Hero';
 
-function LandingPage({ onOpenRegister, onOpenLogin }) {
+function LandingPage({ onOpenRegister, onOpenLogin, onOpenVerify }) {
     return (
-        <PublicLayout onOpenRegister={onOpenRegister} onOpenLogin={onOpenLogin}>
+        <PublicLayout onOpenRegister={onOpenRegister} onOpenLogin={onOpenLogin} onOpenVerify={onOpenVerify}>
             <Hero onOpenRegister={onOpenRegister} />
         </PublicLayout>
     );

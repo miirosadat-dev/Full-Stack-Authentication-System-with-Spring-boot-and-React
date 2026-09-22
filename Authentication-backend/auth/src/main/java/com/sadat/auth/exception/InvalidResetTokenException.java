b0044588ea22f28d5c7c56c09691cd419aeba66d
SADAT MIIRO <miirosadat0@gmail.com>
2026-09-22 18:47:13 +0300

    Commit the Invalid reset token exception

---
+++ Authentication-backend/auth/src/main/java/com/sadat/auth/exception/InvalidResetTokenException.java
@@ -0,0 +1,7 @@
+package com.sadat.auth.exception;
+
+public class InvalidResetTokenException extends RuntimeException {
+    public InvalidResetTokenException(String message) {
+        super(message);
+    }
+}

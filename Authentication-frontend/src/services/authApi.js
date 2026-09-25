3e0b1d9b0809c1290887e72ff6eab492d213de94
SADAT MIIRO <miirosadat0@gmail.com>
2026-09-25 20:19:44 +0300

    Update the authApi and create the .meta.env file

--- Authentication-frontend/src/services/authApi.js
+++ Authentication-frontend/src/services/authApi.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8080/api';
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 async function handleResponse(res) {
     const data = await res.json().catch(() => null);
